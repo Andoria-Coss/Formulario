d0b8016e02aa26e66294b0a362365106930d55ff
Andoria Coss <59074955+Andoria-Coss@users.noreply.github.com>
2022-04-04 15:10:58 -0500

    Add files via upload

---
+++ MuseoAgendar/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Museo La Casa Del Nahual De Nogales Veracruz</title>
+    <link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/estilo.css" media="screen" title="no title">
+  </head>
+  <body>
+
+    <div class="contenedor">
+      <h2>Pase De Entrada Al Museo</h2>
+
+        <div class="contenido">
+
+          <form class="" action="respuesta.php" method="post">
+
+            <div class="informacion">
+
+                  <div class="campo">
+                      <label for="nombre">Nombre
+                          <input type="text" name="nombre" id="nombre">
+                      </label>
+                  </div>
+                  <div class="campo">
+                      <label for="apellido">Apellido
+                          <input type="text" name="apellido" id="apellido">
+                      </label>
+                  </div>
+
+                  <!-- Desde un checkbox-->
+                  <div class="campo">
+                      <label for="notificaciones">Notificar Eventos
+                          <input type="checkbox" name="notificaciones" id="notificaciones">
+                      </label>
+                  </div>
+          </div> <!--.informacion-->
+
+
+                  <!-- Desde array de Checkboxes-->
+                  <div class="cursos">
+                            <h4>Actividad</h4>
+                            <div class="campo">
+                                <label for="html5">Acceso
+                                    <input type="checkbox" name="curso[]" value="Acceso" id="ac">
+                                </label>
+                            </div>
+                            <div class="campo">
+                                <label for="css3">Recorrido Guiado
+                                    <input type="checkbox" name="curso[]" value="Recorrido Guiado" id="rg">
+                                </label>
+                            </div>
+                            <div class="campo">
+                                <label for="javascript">Evento
+                                    <input type="checkbox" name="curso[]" value="Evento" id="ev">
+                                </label>
+                            </div>
+                  </div>
+
+
+                  
+                  <div class="especializacion">
+                    <h4>Horario</h4>
+
+                        <select name="area" value="-Any-">
+                            <option>- Selecciona un horario -</option>
+                            <option value="ma">Mañana (10:00-12:00 Hrs)</option>
+                            <option value="ta">Tarde (12:00-18:00 Hrs)</option>
+                            <option value="no">Noche (19:00-Hora de cierre)</option>
+                        </select>
+                  </div>
+
+
+
+            <div class="tipo_curso">
+                  <h4>Tipo De Visita</h4>
+                  <?php  $opciones = array(
+                      'pres' => 'Presencial',
+                      'online' => 'Virtual'
+                  );?>
+
+
+                  <?php foreach($opciones as $key => $opcion) {
+                    echo "<div class='campo'>";
+                    echo "<input type='radio' name='opciones' value='$key' > $opcion";
+                    echo "</div>";
+                  } ?>
+            </div>
+
+            <div class="textarea">
+                  <div class="campo">
+                    <label for="mensaje"> Mensaje Al Director <br/>
+                        <textarea name="mensaje" rows="8" cols="40" id="mensaje"></textarea>
+                    </label>
+                    <input type="submit">
+                  </div>
+            </div>
+          </form>
+
+
+        </div>
+    </div>
+
+
+
+
+  </body>
+</html>
